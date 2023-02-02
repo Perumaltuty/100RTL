59ca75a1136360b6ab554e53f0fd51a6208222c7
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-02 14:42:40 +0530

    Create gray_to_binary.v

---
+++ Combinational/gray_to_binary/gray_to_binary.v
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+module gray_to_binary(input[3:0] G,output [3:0] bin);
+  
+assign bin[3] = G[3];
+assign bin[2] = G[3] ^ G[2];
+assign bin[1] = G[3] ^ G[2] ^ G[1];
+assign bin[0] = G[3] ^ G[2] ^ G[1] ^ G[0];
+
+endmodule
