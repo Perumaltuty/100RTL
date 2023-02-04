82c6f7caf98b08c2f7b72145d023f54fc759c723
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-04 15:16:43 +0530

    Create Bit_equality_tb.v

---
+++ Combinational/equality/Bit_equality_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+module Bit_equality_tb();
+    reg [1:0] A = 0, B = 0 ;
+    wire z;
+
+   Bit_equality  tb ( .A(A), .B(B), .z(z) );
+    always @(A or B)
+    begin
+        A = 00;
+        B = 00;
+        #10;
+        A = 01;
+        B = 01;
+        #10;
+        A =10;
+        B = 00;
+        #10;
+        A = 11;
+        B = 10;
+        #10;
+    end
+
+    
+endmodule
+
