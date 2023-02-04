975d581df738da49644564e4e8d75717c56efd09
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-04 11:03:50 +0530

    Create SISO_TB.v

---
+++ Sequential/SISO/SISO_TB.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+module SISO_TB;
+
+reg clk;
+reg clear;
+reg si;
+wire so;
+SISO uut (.clk(clk), .clear(clear),.si(si),.so(so));
+initial begin
+clk = 0;
+clear = 0;
+si = 0;
+#5 clear=1'b1;
+#5 clear=1'b0;
+#10 si=1'b1;
+#10 si=1'b0;
+#10 si=1'b0;
+#10 si=1'b1;
+#10 si=1'b0;
+#10 si=1'bx;
+end
+always #5 clk = ~clk;
+initial #150 $finish;
+endmodule
