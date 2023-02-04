1cc73024547c6730ef4b0feffdf853f328daed51
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-04 11:03:18 +0530

    Create SISO.v

---
+++ Sequential/SISO/SISO.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+module SISO(clk,clear,si,so);
+input clk,si,clear;
+output so;
+reg so;
+reg [3:0] tmp = 0;
+always @(posedge clk )
+begin
+if (clear)
+tmp <= 4'b0000;
+else
+tmp <= tmp << 1;
+tmp[0] <= si;
+so = tmp[3];
+end
+endmodule
