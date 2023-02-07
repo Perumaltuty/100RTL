b652d90dceb8564daf1d5c3aac31a05e62f14e37
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-07 17:00:54 +0530

    Create modN_ctr.v

---
+++ Sequential/modN_ctr/modN_ctr.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+module modN_ctr
+#(parameter N = 10,
+parameter WIDTH = 4)
+(input clk,
+input rstn,
+output reg [WIDTH-1:0] out);
+always @ (posedge clk)begin
+if(!rstn) begin
+out <=0;
+end else begin
+if(out == N-1)
+out <= 0;
+else 
+out <= out+1;
+end
+end
+endmodule
