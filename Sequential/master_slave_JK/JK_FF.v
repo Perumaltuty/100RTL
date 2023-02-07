e26c2513e55225c7314c326713d048bc58a3fde5
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-07 09:29:21 +0530

    Create JK_FF.v

---
+++ Sequential/master_slave_JK/JK_FF.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+module JK_FF(j,k,clk,q,q_bar);
+input j,k,clk;
+output  q,q_bar;
+reg q;
+assign q_bar= ~q;
+always @(posedge clk)
+begin
+
+case({j,k})
+2'b00: q<=q;
+2'b01: q<=0;
+2'b10: q<=1;
+2'b11: q<=~q;
+endcase
+end
+endmodule
+
